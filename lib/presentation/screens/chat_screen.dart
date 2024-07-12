0bb6b70bbab822f749f760d2b3079561806f09a2
Maria CR <carc1403@gmail.com>
2024-07-11 20:28:06 -0600

    proyecto yes_no

---
+++ lib/presentation/screens/chat_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(leading: const Padding(padding: EdgeInsets.all(4.0),
+      child: CircleAvatar(
+        backgroundImage: NetworkImage('https://cdn.icon-icons.com/icons2/1494/PNG/512/user_102890.png'),
+        ),
+      ),
+      title: const Text('Maria'),
+      centerTitle: false,
+      ),
+      body: const _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView();
+
+  @override
+  Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              controller: chatProvider.chatScrollController,
+              itemCount: chatProvider.messages.length,
+              itemBuilder: (context, index){
+                final message = chatProvider.messages[index];
+                return (message.fromWho == FromWho.her) 
+                  ? HerMessageBubble(
+                    message: message
+                    )
+                  : MyMessageBubble(
+                    message: message
+                  );
+              }),
+            ),  
+            MessageFieldBox(onValue: chatProvider.sendMessage,)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
